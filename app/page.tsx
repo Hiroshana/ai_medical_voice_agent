d0f5557ae473126d94cbd2fc2c90684bd53126a7
Hiroshana <hiroshanawanni@gmail.com>
2025-07-27 10:37:23 +1000

    Installed Drizzle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import HeroSectionOne from "@/components/HeroSectionOne";
-import SectionTwo from "@/components/SectionTwo";
+import SectionTwo from "@/components/FeatureSection";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
